7462da74e00f7df4f6565f1940f128778f04c816
hoseong511 <thdghtjd115@naver.com>
2022-08-18 01:17:17 +0900

    Update hossong : added init_pipe, init_info

--- srcs/ctrl_excution.c
+++ srcs/ctrl_excution.c
@@ -6,7 +6,7 @@
 /*   By: hossong <hossong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 17:27:01 by hossong           #+#    #+#             */
-/*   Updated: 2022/08/17 19:31:36 by hossong          ###   ########.fr       */
+/*   Updated: 2022/08/18 01:01:10 by hossong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,45 +35,73 @@ void	do_child_proc(t_data *data, t_list *c_node, int depth)
 	(void)depth;
 	redirection();
 	excute_cmd();
-	sleep(1);
+	sleep(2);
 	exit(1);
 }
 
 void	do_parent_proc(t_data *data, int depth)
 {
-	int	status;
-
 	(void)data;
 	(void)depth;
-	wait(&status);
-	printf("parent, exit_status : %d\n", WEXITSTATUS(status));
+	if (data->cmd_cnt > 1)
+	{
+		if (depth % 2 == 0)
+			close(data->info->pipe[0].fd[1]);
+		else
+		{
+			close(data->info->pipe[0].fd[0]);
+			close(data->info->pipe[1].fd[1]);
+		}
+	}
+	if (depth == data->cmd_cnt - 1)
+	{
+		while (wait(&data->info->status) != -1)
+			;
+		printf("parent, exit_status : %d\n", WEXITSTATUS(data->info->status));
+		//close(data->info->pipe[1].fd[0]);
+	}
+}
+
+void	init_pipe(t_proc *info, int depth)
+{
+	if (depth % 2 == 0 && pipe(info->pipe[0].fd) != 0)
+		ft_perror("pipe", errno);
+	else if (depth % 2 && pipe(info->pipe[1].fd) != 0)
+		ft_perror("pipe", errno);
+}
+
+t_proc	*init_info(void)
+{
+	t_proc	*new;
+
+	new = (t_proc *)malloc(sizeof(t_proc));
+	if (!new)
+		ft_error("ERROR: Malloc Error\n");
+	ft_memset(new, 0, sizeof(t_proc));
+	return (new);
 }
 
 void	execution(t_data *data)
 {
 	t_list	*cmdlist;
-	t_proc	*info;
 	int		depth;
 
-	info = (t_proc *)malloc(sizeof(t_proc));
-	if (!info)
-		ft_error("ERROR: Malloc Error\n");
-	ft_memset(info, 0, sizeof(t_proc));
-	data->info = info;
+	data->info = init_info();
 	cmdlist = data->cmdlist;
 	depth = 0;
 	while (cmdlist && depth < data->cmd_cnt)
 	{
-		info->pid = fork();
-		if (depth == 0 && pipe(info->pipe[0].fd))
-			ft_perror("pipe", errno);
-		if (info->pid > 0)
+		data->info->pid = fork();
+		if (data->cmd_cnt > 1)
+			init_pipe(data->info, depth);
+		if (data->info->pid > 0)
 			do_parent_proc(data, depth);
-		else if (info->pid == 0)
+		else if (data->info->pid == 0)
 			do_child_proc(data, (t_list *)cmdlist->content, depth);
 		else
 			ft_perror("fork error", errno);
 		cmdlist = cmdlist->next;
 		depth++;
 	}
+	data->cmd_cnt = 0;
 }
