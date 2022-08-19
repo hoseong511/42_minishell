4153b8f4c46e5e195366738b9418a1b999ece0c6
Nammi Kim <0skyblue@naver.com>
2022-08-19 19:16:10 +0900

    Update namkim : add built-in functions

--- srcs/export_n_env.c
+++ srcs/export_n_env.c
@@ -6,28 +6,29 @@
 /*   By: namkim <namkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 10:54:24 by namkim            #+#    #+#             */
-/*   Updated: 2022/08/19 14:57:22 by namkim           ###   ########.fr       */
+/*   Updated: 2022/08/19 17:21:19 by namkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/main.h"
 
-int	check_args(char	*target, char chr)
-{
-	int		len;
-	char	*divider;
+// int	check_args(char	*target, char chr)	- 현재 안 씀
+// {
+// 	int		len;
+// 	char	*divider;
 
-	len = get_env_len(target);
-	if (len < 1)
-		return (FALSE);
-	if (chr == 'e')
-	{
-		divider = ft_strchr(target, '=');
-		if (divider == NULL || *divider == '\0')
-			return (FALSE);
-	}
-	return (TRUE);
-}
+// 	len = get_env_len(target);
+// 	if (len < 1)
+// 		return (FALSE);
+// 	if (chr == 'e')
+// 	{
+// 		divider = ft_strchr(target, '=');
+// 		if (divider == NULL || *divider == '\0')
+// 			return (FALSE);
+// 	}
+// 	return (TRUE);
+// }
+
 //expansion의 경우 원래 (space), ':', '/' 등이 구분문자로 기능, But 우리는 : / 등을 해석하지 않기 때문에 그냥 space로만 expansion 할 것임!
 //expansion의 경우 ""와 ''는 전부 shell prompt의 expansion 규칙을 따르는 듯 하다.
 //복수의 argument가능, expansion 있음. value로 빈문자열 입력 가능. '='를 입력하지 않으면 무효처리
@@ -59,10 +60,11 @@ void	ft_export(char **args, t_data *data)
 			if (idx >= 0)
 			{
 				free(data->envlist[idx]);
-				data->envlist[idx] = args[i];
+				data->envlist[idx] = ft_strdup(args[i]);	//복제해서 넣는다
 			}
 			else
 				add_env_to_envlist(args[i], data);
+			free(key);
 		}
 		//else : 로컬 변수를 쓴다면...
 	}
@@ -89,6 +91,8 @@ void	ft_env(char **args, t_data *data)
 			printf("declare -x ");
 		printf("%s\n", *envp++);
 	}
+	if (args[0][2] == 'n')
+		printf("_=/usr/bin/%s\n", args[0]);
 }
 
 //envp사이즈 관리할까...? 관리하는게 편할 것 같음.
@@ -98,7 +102,7 @@ void	ft_env(char **args, t_data *data)
 //envp에서 키 값이 같은 애가 있는지 찾는다
 //있으면? 삭제: free하고 memcpy...
 //실험 필요
-void	unset(char **args, t_data *data)
+void	ft_unset(char **args, t_data *data)
 {
 	int		i;
 	int		e;
@@ -127,6 +131,82 @@ void	unset(char **args, t_data *data)
 //이 때 코드가 바로 exit_status
 
 //pwd : argument를 몇개 넣든 무시하는 것 같다
+//대문자로도 된다! < 모든 대소문자의 조합으로 가능하다... 대체 왜...?
+//알고보니...? 모든 명령어는 대소문자 관계없이 실행이 가능했다... 왜????
+//touch B < 파일은 찾을 수 없는데, cat은 가능하다... 왜????
+void	ft_pwd(char **args)
+{
+	char	*res;
+
+	if (!args || !args[0])
+		return ;	//or error
+	else
+	{
+		res = getcwd(NULL, 0);
+		if (!res)
+			ft_error("Couldn't get working directory\n");
+		printf("%s\n", res);
+		free(res);
+	}
+
+}
 //a=$(pwd) < 이런 식의 사용이 가능하지만! 우리는 괄호를 해석하지 않는다...
-//대문자로도 된다!
 //cd 할 때마다 env에 PWD와 OLDPWD가 업데이트 된다 (bash는 OLDPWD 없음)
+
+//cd - : 이전 위치로(할 필요 없음)
+//cd ~ : home directory로(할 필요 없음)
+//cd with no args : home directory로(할 필요 없지 않을까...?)
+//위치로 옮기고, env update
+void	ft_cd(char **args, t_data *data)
+{
+	char	*path;
+	char	*pwd;
+	int		sign;
+
+	if (!args[1])
+		path = match_env("HOME", data->envlist);	//확인할 것
+	else
+		path = args[1];
+	sign = chdir(path);
+	if (sign == 0)
+	{
+		free(path);
+		pwd = getcwd(NULL, 0);
+		if (!pwd)
+			ft_error("Couldn't get working directory\n");
+		path = ft_strjoin("PWD=", pwd);
+		ft_export(&path, data);
+		free(pwd);
+		free(path);
+	}
+	else
+	{
+		printf("cd: %s: No such file or directory\n", args[1]);
+		exit(1);
+	}
+}
+
+//여기서 모든 메모리 해제를 넣어줘야 하는지...? 어차피 exit가 모두 처리할건데도?
+void	ft_exit(char **args)
+{
+	printf("exit\n");
+	exit(0);
+}
+
+void	ft_echo(char **args)
+{
+	int	i;
+
+	i = 1;
+	if (ft_strncmp(args[i], "-n", 3) == 0)
+		i++;
+	while (args[i + 1])
+	{
+		write(1, args[i], ft_strlen(args[i]));
+		write(1, " ", 1);
+		i++;
+	}
+	write(1, args[i], ft_strlen(args[i]));
+	if (ft_strncmp(args[1], "-n", 3) != 0)
+		write(1, '\n', 1);
+}
--- srcs/signal.c
+++ srcs/signal.c
@@ -6,15 +6,20 @@
 /*   By: namkim <namkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 22:13:27 by namkim            #+#    #+#             */
-/*   Updated: 2022/08/17 19:54:26 by namkim           ###   ########.fr       */
+/*   Updated: 2022/08/19 15:09:26 by namkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/main.h"
 /*
-Ctrl + C : SIGINT
-Ctrl + D : exit shell... exit shell 하는 건 너무 많은데..?ㅠㅠ
+Ctrl + C : SIGINT - interactive / 실행 중 모두 exit status (1);
+Ctrl + D :
+	interactive의 경우 - 프롬프트에 글자가 남아있을 때 : 아무 문제 없음
+	글자가 없을 때 : exit를 찍고 나간다(정상종료->exit status == 0)
+	프로그램이 실행 중일 때 : 아무 작동 안하는 것 같음.
 Ctrl+D is not a signal, it's EOF (End-Of-File).
 It closes the stdin pipe. If read(STDIN) returns 0 (원래는 이렇다)
-Ctrl + \ : do nothing : sigquit
+Ctrl + \ : do nothing : sigquit <- 이건 그냥 재우면 됨...!
+대체 왜? g_exit_status가 필요하지?
+자식프로세스가 부모프로세스에 오류를 전달하기 위해?
 */
