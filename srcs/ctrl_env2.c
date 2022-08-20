52c9ce47020714a399eb7255ba1a3f3c0ea6f617
hoseong511 <thdghtjd115@naver.com>
2022-08-20 18:21:34 +0900

    Update hossong : added 130

--- srcs/ctrl_env2.c
+++ srcs/ctrl_env2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ctrl_env2.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: namkim <namkim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: hossong <hossong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 15:46:57 by namkim            #+#    #+#             */
-/*   Updated: 2022/08/17 19:53:26 by namkim           ###   ########.fr       */
+/*   Updated: 2022/08/20 17:53:23 by hossong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	get_env_len(char *target)
 	int	i;
 
 	i = 0;
+	if (target[0] == '?')
+		return (1);
 	while (is_valid_env_name(target[i], i) == TRUE)
 		i++;
 	return (i);
@@ -28,12 +30,17 @@ void	replace_env(char **target, int start, int keysize, char **envp)
 	char	*keystr;
 
 	str = *target;
-	keystr = ft_strndup(str + start, keysize);
-	if (!keystr)
-		ft_error("Malloc error\n");
-	*target = replace_key_to_value(str, start, keystr, envp);
-	free(str);
-	free(keystr);
+	if (envp == NULL)
+		*target = replace_key_to_value(str, start, "?", NULL);
+	else
+	{
+		keystr = ft_strndup(str + start, keysize);
+		if (!keystr)
+			ft_error("Malloc error\n");
+		*target = replace_key_to_value(str, start, keystr, envp);
+		free(str);
+		free(keystr);
+	}
 }
 
 int	count_env(char *str, char chr)
@@ -63,4 +70,35 @@ int	count_env(char *str, char chr)
 		i++;
 	}
 	return (res);
+}
+
+void	do_replace(t_list *com, int *i, int *j, char *target)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (target[i + j])
+	{
+		if (target[i + j] == '\'' || target[i + j] == '\"')
+		{
+			if (j == 0)
+			{
+				j = get_quote_end_idx(target, i);
+				make_component(&com, target + i, (j - i + 1));
+				process_quote(ft_lstlast(com), envp, target[i]);//
+				i = j + 1;
+			}
+			else
+			{
+				make_component(&com, target + i, j);
+				process_non_quote(ft_lstlast(com), envp);//
+				i += j;
+			}
+			j = 0;
+		}
+		else
+			j++;
+	}
 }
