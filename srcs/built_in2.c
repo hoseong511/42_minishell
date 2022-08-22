8fd98a6e53b7c86b4d8089a5dcecd57dc7184f4a
Nammi Kim <0skyblue@naver.com>
2022-08-22 15:02:50 +0900

    Refactoring namkim : fix cd funcs

--- srcs/built_in2.c
+++ srcs/built_in2.c
@@ -6,7 +6,7 @@
 /*   By: namkim <namkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 12:57:34 by namkim            #+#    #+#             */
-/*   Updated: 2022/08/22 12:42:02 by namkim           ###   ########.fr       */
+/*   Updated: 2022/08/22 14:59:20 by namkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,14 +43,25 @@ char	*ft_getpwd(void)
 	return (pwd);
 }
 
+static void	ft_cd_error(char *arg)
+{
+	printf("cd: %s: No such file or directory\n", arg);
+	g_status = 1;
+}
+
 void	ft_cd(char **args, t_data *data)
 {
 	char	*path;
 	char	*pwd;
+	int		home;
 
 	pwd = ft_getpwd();
+	home = FALSE;
 	if (!args[1])
+	{
 		path = match_env("HOME", data->envlist);
+		home = TRUE;
+	}
 	else
 		path = args[1];
 	if (chdir(path) == 0)
@@ -62,65 +73,7 @@ void	ft_cd(char **args, t_data *data)
 		free(pwd);
 	}
 	else
-	{
-		printf("cd: %s: No such file or directory\n", args[1]);
-		g_status = 1;
-	}
-	free(path);//
-}
-
-static int	check_exit_args(char *arg)
-{
-	int	i;
-
-	i = 0;
-	if (arg[i] == '+' || arg[i] == '-')
-		i++;
-	while (ft_isdigit(arg[i]) == TRUE)
-		i++;
-	if (arg[i] == '\0')
-		return (TRUE);
-	else
-		return (FALSE);
-}
-
-void	ft_exit(char **args)
-{
-	int	errcode;
-	int	i;
-
-	errcode = 0;
-	i = 1;
-	printf("exit\n");
-	if (!args)
-		exit(errcode);
-	while (args[i])
-	{
-		if (check_exit_args(args[i]) == FALSE)
-			ft_error3("exit", ": numeric argument required\n", 255);
-		i++;
-	}
-	if (i > 2)
-		ft_error2("exit", ": too many arguments\n");
-	else if (i == 2)
-		errcode = ft_atoi(args[1]);
-	exit(errcode);
-}
-
-void	ft_echo(char **args)
-{
-	int	i;
-
-	i = 1;
-	if (ft_strncmp(args[i], "-n", 3) == 0)
-		i++;
-	while (args[i + 1])
-	{
-		write(1, args[i], ft_strlen(args[i]));
-		write(1, " ", 1);
-		i++;
-	}
-	write(1, args[i], ft_strlen(args[i]));
-	if (ft_strncmp(args[1], "-n", 3) != 0)
-		write(1, "\n", 1);
+		ft_cd_error(path);
+	if (home == TRUE)
+		free(path);
 }
